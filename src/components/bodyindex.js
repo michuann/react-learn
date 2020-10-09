067152ee8cce664ee97ed3822cc9e42b75501aaa
michuan <michuan@4paradigm.com>
2020-10-09 15:34:21 +0800

    08-01

--- src/components/bodyindex.js
+++ src/components/bodyindex.js
@@ -1,32 +1,24 @@
 import React from "react";
 
 export default class BodyIndex extends React.Component{
-
-    componentWillMount() {
-        //定义你的逻辑即可
-        console.log("BodyIndex - componentWillMount");
-    }
-
-    componentDidMount() {
-        console.log("BodyIndex - componentDidMount");
+    // 构造函数
+    constructor() {
+        super();  //调用基类的所有初始化方法
+        this.state = {username : "Parry"};
     }
 
     render() {
-        var userName = 'Parry';
-        var boolInput = false;
 
-        var html = 'IMOOC&nbsp;LESSON';
+        setTimeout(()=>{
+            //更改 state 的时候
+            this.setState({username : "IMOOC"});
+        },4000);
 
-        //有用户名显示用户名，没用户提示未登录
-
+        //component
         return (
             <div>
                 <h2>页面的主体内容</h2>
-                <p>{userName=='' ? '用户还未登录' : '用户名：' + userName}</p>
-                <p><input type='button' value='默认按钮' disabled={boolInput}/></p>
-                {/*// //*/}
-                <p>{html}</p>{/*需要进行 Unicode 的转码*/}
-                <p dangerouslySetInnerHTML={{__html: html}}></p>
+                <p>{this.state.username}</p>
             </div>
         );
     }
