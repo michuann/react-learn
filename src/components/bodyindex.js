0719a88c66359c1927338f2392b9f807fdb69cb0
michuan <michuan@4paradigm.com>
2020-10-09 20:22:13 +0800

    08-05 修改dom节点属性refs

--- src/components/bodyindex.js
+++ src/components/bodyindex.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import BodyChild from "./bodychild";
 
@@ -18,6 +19,14 @@ export default class BodyIndex extends React.Component{
 
     changeUserInfo(age) {
         this.setState({age : age});
+        // 第一种方式
+        // var mySubmitButton = document.getElementById("submitButton");
+        // console.log(mySubmitButton);
+        // ReactDOM.findDOMNode(mySubmitButton).style.color = 'red';
+        // 第二种方式
+        console.log(this.refs.submitButton);
+        ReactDOM.findDOMNode(this.refs.submitButton).style.color = 'red';
+
     };
 
     handleChildValueChange(event) {
@@ -35,7 +44,7 @@ export default class BodyIndex extends React.Component{
                 <h2>页面的主体内容</h2>
                 <p>接收父页面的属性：userid: {this.props.userid} username: {this.props.username}</p>
                 <p>age: {this.state.age}</p>
-                <input type='button' value='提交' onClick={this.changeUserInfo.bind(this, 99)}/>
+                <input id='submitButton' ref='submitButton' type='button' value='提交' onClick={this.changeUserInfo.bind(this, 99)}/>
                 <BodyChild {...this.props} id={4} handleChildValueChange={this.handleChildValueChange.bind(this)}/>
             </div>
         )
