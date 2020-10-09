9864259e9190ab54081096e52901fb03bd06929e
michuan <michuan@4paradigm.com>
2020-10-09 15:36:53 +0800

    08-01 update

--- src/components/bodyindex.js
+++ src/components/bodyindex.js
@@ -4,21 +4,24 @@ export default class BodyIndex extends React.Component{
     // 构造函数
     constructor() {
         super();  //调用基类的所有初始化方法
-        this.state = {username : "Parry"};
+        this.state = {
+            username : "Parry",
+            age : 20
+        }; // 初始化赋值
     }
 
     render() {
 
         setTimeout(()=>{
             //更改 state 的时候
-            this.setState({username : "IMOOC"});
+            this.setState({username : "IMOOC", age : 30});
         },4000);
 
         //component
         return (
             <div>
                 <h2>页面的主体内容</h2>
-                <p>{this.state.username}</p>
+                <p>{this.state.username} {this.state.age}</p>
             </div>
         );
     }
