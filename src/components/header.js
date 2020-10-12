0fdef8ff10cc1f1552f1b639c20232a21df54505
michuan <michuan@4paradigm.com>
2020-10-12 20:09:48 +0800

    09-02 css内联样式表达式

--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,35 @@ import '../css/style.css';
 
 export default class ComponentHeader extends React.Component{
 
+    constructor() {
+        super();
+        this.state = {
+            miniHeader: false  //默认加载高的头部(非mini头部)
+        }
+    };
+
+    switchHeader(){
+        this.setState({
+            miniHeader: !this.state.miniHeader
+            });
+        console.log(this.state.miniHeader)
+    };
+
     render() {
         // 内联样式。  设置头部css样式
         const styleComponentHeader = {
             header: {
                 backgroundColor: "#333333",
                 color: "#FFFFFF",
-                "padding-top": "15px",
-                paddingBottom: "15px",
+                "padding-top": (this.state.miniHeader) ? "3px" : "15px",
+                paddingBottom: (this.state.miniHeader) ? "3px" : "15px",
             },
             // 还可以定义其他的样式
         };
 
         return (
-            <header style={styleComponentHeader.header} className="smallFontSize">
+            <header style={styleComponentHeader.header} className="smallFontSize"
+            onClick={this.switchHeader.bind(this)}>
                 <h1>这里是头部</h1>
             </header>
         );
